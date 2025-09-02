09a067f80f0877315753d739b6a900e6b1c53395
soldadodigital <eldcapdom09@gmail.com>
2025-09-02 13:39:30 -0300

    Add files via upload

---
+++ automaçao-pyton/www.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# abra o navegador
+navegador = webdriver.Chrome()
+
+
+
+#entre no site
+navegador.get("https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
+
+time.sleep(10)
+
+#selecione o elemento na tela
+navegador.find_element("class name", "MuiBox-root" )
+
+
+navegador.find_element("generic")
+time.sleep(10)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# abra o navegador
+navegador = webdriver.Chrome()
+
+
+
+#entre no site
+navegador.get("https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
+
+time.sleep(10)
+
+#selecione o elemento na tela
+navegador.find_element("class name", "MuiBox-root" )
+
+
+navegador.find_element("generic")
+time.sleep(10)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# abra o navegador
+navegador = webdriver.Chrome()
+
+
+
+#entre no site
+navegador.get("https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
+
+time.sleep(10)
+
+#selecione o elemento na tela
+navegador.find_element("class name", "MuiBox-root" )
+
+
+navegador.find_element("generic")
+time.sleep(10)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+# abra o navegador
+navegador = webdriver.Chrome()
+
+
+
+#entre no site
+navegador.get("https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
+
+time.sleep(10)
+
+#selecione o elemento na tela
+MuiIconButton-root = navegador.find_element("class name", "MuiIconButton-root " )
+
+#clicar no elemento
+MuiIconButton-root.click()
+
+time.sleep(10)
